cbd93d3e7f5392338c6d958f64e714af7878db33
Prajwal-454 <177189614+Prajwal-454@users.noreply.github.com>
2026-08-31 21:11:31 +0530

    feat: add @vercel/analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { registerSW } from "virtual:pwa-register";
 
+import { Analytics } from "@vercel/analytics/react";
+
 import App from "./App";
 import "./index.css";
 
@@ -12,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
